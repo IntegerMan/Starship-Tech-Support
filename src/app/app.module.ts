765f80de0f3d3ef632ef539f31a47fd78deab7dd
Matt Eland <Matt.Eland@GMail.com>
2019-08-10 23:49:00 -0400

    Build process refinement, removed Redux router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,9 +45,11 @@ import {routerReducer, RouterState, StoreRouterConnectingModule} from '@ngrx/rou
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
     }),
+/*
     StoreRouterConnectingModule.forRoot({
       routerState: RouterState.Minimal
     })
+*/
   ],
   providers: [GameStateStore],
   bootstrap: [AppComponent]
